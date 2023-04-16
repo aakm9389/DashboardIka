0d94a85e9c1ee52880bfcdfb9a555bda497e2b1d
aakm9389 <aakm9389@gmail.com>
2023-04-16 15:53:03 +0000

    mon cmmit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
+import { VenteparmoisComponent } from './statistique/VenteParMois/venteparmois/venteparmois.component';
+import { AchatParmoisComponent } from './statistique/achat-parmois/achat-parmois.component';
+import { CommandeParMoisComponent } from './statistique/commande-par-mois/commande-par-mois.component';
+import { NombreClientComponent } from './statistique/nombre-client/nombre-client.component';
+import { RapportComponent } from './rapport/rapport.component';
+import { CanauxComponent } from './rapport/canaux/canaux.component';
+import { UtilisateurComponent } from './rapport/utilisateur/utilisateur.component';
+import { ConversionComponent } from './rapport/conversion/conversion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatistiqueComponent,
+    VenteparmoisComponent,
+    AchatParmoisComponent,
+    CommandeParMoisComponent,
+    NombreClientComponent,
+    RapportComponent,
+    CanauxComponent,
+    UtilisateurComponent,
+    ConversionComponent
   ],
   imports: [
     BrowserModule,
